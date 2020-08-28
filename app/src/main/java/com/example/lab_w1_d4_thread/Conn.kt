9920bcbdd0067739e81cfb025ffec59c10fbd088
Matias Jalava <matias@elm.fi>
2020-08-28 12:07:08 +0300

    initial commit

---
+++ app/src/main/java/com/example/lab_w1_d4_thread/Conn.kt
@@ -0,0 +1,54 @@
+package com.example.lab_w1_d4_thread
+
+import android.os.Handler
+import android.util.Log
+import java.io.InputStream
+import java.lang.Exception
+import java.net.HttpURLConnection
+import java.net.URL
+
+class Conn (
+    mHand: Handler,
+    val fname: String,
+    val lname: String) : Runnable {
+
+        private val myHandler = mHand
+
+        override fun run() {
+            Log.d("Thread-lab", "in run func")
+            try {
+                // val myUrl = URL("https://a.com/post.php")
+                val myUrl = URL("https://www.w3.org/TR/PNG/iso_8859-1.txt")
+                val myConn = myUrl.openConnection() as HttpURLConnection
+
+                // myConn.requestMethod = "POST"
+                myConn.requestMethod = "GET"
+                myConn.doOutput = true
+
+                /*
+                val ostream = myConn.getOutputStream()
+
+                ostream.bufferedWriter().use {
+                    it.write("fn=${fname}&ln=${lname}")
+                }
+                */
+
+                val istream: InputStream = myConn.getInputStream()
+                val allText = istream.bufferedReader().use {
+                    it.readText()
+                }
+
+                val result = StringBuilder()
+                result.append(allText)
+                val str = result.toString()
+
+                val msg = myHandler.obtainMessage()
+                msg.what = 0
+                msg.obj = str
+                myHandler.sendMessage(msg)
+
+            }catch(e: Exception){
+                Log.d("Thread-lab", "$e")
+            }
+        }
+    }
---
+++ app/src/main/java/com/example/lab_w1_d4_thread/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.lab_w1_d4_thread
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.util.Log
+
+
+class MainActivity : AppCompatActivity() {
+
+    // Image link: https://i.redd.it/qs0v7vt9b7i51.jpg
+
+    private val myHandler: Handler = object :
+            Handler(Looper.getMainLooper()) {
+        override fun handleMessage(inputMessage: Message) {
+            if (inputMessage.what== 0) {
+                txt_network.text= inputMessage.obj.toString()
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("Thread-lab","onCreate")
+        if (isNetworkAvailable()) {
+            val myRunnable = Conn(
+                    myHandler,
+                    "John",
+                    "Doe")
+            val myThread = Thread(myRunnable)
+            myThread.start()
+        }
+    }
+
+    // from here: http://chintanrathod.com/kotlin-check-internet-connection-available-android/
+    private fun isNetworkAvailable(): Boolean {
+        Log.d("Thread-lab","isNetworkAvailable")
+        val connectivityManager = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
+        return activeNetwork?.isConnectedOrConnecting == true }
+}
+
