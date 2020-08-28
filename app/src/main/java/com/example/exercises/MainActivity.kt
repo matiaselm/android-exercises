6550318c5788401b8288ac8d2ee621d7ff10080a
Matias Jalava <matias@elm.fi>
2020-08-28 15:43:00 +0300

    changes to master

--- app/src/main/java/com/example/exercises/MainActivity.kt
+++ app/src/main/java/com/example/exercises/MainActivity.kt
@@ -1,81 +1,17 @@
 package com.example.exercises
 
-import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.net.ConnectivityManager
-import android.net.NetworkInfo
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.os.Message
-import android.util.Log
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.io.InputStream
-import java.lang.Exception
-import java.net.HttpURLConnection
-import java.net.URL
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private val myHandler: Handler = object :
-            Handler(Looper.getMainLooper()) {
-        override fun handleMessage(inputMessage: Message) {
-            if (inputMessage.what== 0) {
-                txt_network.text= inputMessage.obj.toString()
-            }
-        }
-    }
-
-    // Loads an image from the URL and converts it to Bitmap, returns bitmap or null
-    private suspend fun loadBitmap(imageUrl: URL): Bitmap? =
-        withContext(Dispatchers.IO){
-        try {
-            val myConn = imageUrl.openConnection() as HttpURLConnection
-
-            myConn.requestMethod = "GET"
-
-            val inputStream: InputStream = myConn.inputStream
-
-            return@withContext BitmapFactory.decodeStream(inputStream)
-        }catch(e: Exception){
-            Log.d("Coroutine-lab", "$e")
-            return@withContext null
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("Coroutine-lab","onCreate")
 
-        if (isNetworkAvailable()) {
-            val imgUrl = URL("https://i.redd.it/qs0v7vt9b7i51.jpg")
+        txt_network.text = getString(R.string.branch_master)
 
-            lifecycleScope.launch(Dispatchers.Main){
-                val imgBitmap = loadBitmap(imgUrl)
-                imageView.adjustViewBounds
-                imageView.setImageBitmap(imgBitmap)
-            }
-
-            val myRunnable = Conn(
-                    myHandler)
-
-            val myThread = Thread(myRunnable)
-            myThread.start()
         }
     }
-
-    // from here: http://chintanrathod.com/kotlin-check-internet-connection-available-android/
-    private fun isNetworkAvailable(): Boolean {
-        Log.d("Coroutine-lab","isNetworkAvailable")
-        val connectivityManager = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
-        return activeNetwork?.isConnectedOrConnecting == true }
-}
 
