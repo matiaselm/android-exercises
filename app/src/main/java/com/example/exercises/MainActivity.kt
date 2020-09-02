be6e8d205499714e3ed77cd8eafbceaf708582ce
Matias Jalava <matias@elm.fi>
2020-09-02 14:34:26 +0300

    going to switch android.location to google location API

--- app/src/main/java/com/example/exercises/MainActivity.kt
+++ app/src/main/java/com/example/exercises/MainActivity.kt
@@ -2,26 +2,44 @@ package com.example.exercises
 
 
 import android.content.Context
+import android.content.pm.PackageManager
 import android.location.LocationManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
-    val lm = getSystemService(Context.LOCATION_SERVICE) as
-            LocationManager
+    // val lm = getSystemService(Context.LOCATION_SERVICE) as LocationManager  // Supposed to change to google play location api
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*      Let's change this to google play location API
+        if ((Build.VERSION.SDK_INT >= 23 &&
+                    ContextCompat.checkSelfPermission(
+                        this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION
+                    ) !=
+                    PackageManager.PERMISSION_GRANTED)
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                0
+            )
+        }
 
         lm.requestLocationUpdates(
             LocationManager.GPS_PROVIDER,
             60 * 1000,
             50f,
             this
-        )
+        )*/
 
-        }
     }
+}
 
