09210ee655d1e28632b6def0ce3e17f8ff7782d8
Matias Jalava <matias@elm.fi>
2020-09-07 10:58:04 +0300

    managed to get data out of the sensor

--- app/src/main/java/com/example/exercises/MainActivity.kt
+++ app/src/main/java/com/example/exercises/MainActivity.kt
@@ -15,14 +15,17 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     /*source: https://expertise.jetruby.com/how-to-implement-motion-sensor-in-a-kotlin-app-b70db1b5b8e5 */
     private lateinit var sensorManager: SensorManager
-    private lateinit var accelerometer: Sensor
+    private var accelerometer: Sensor? = null
 
-    private lateinit var accelGravity: Array<Double>
-    private lateinit var accelLin: Array<Double>
+    private lateinit var accelGravity: MutableMap<Int, Double?>
+    private lateinit var accelLin: MutableMap<Int, Double?>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        accelGravity = mutableMapOf<Int, Double?>()
+        accelLin = mutableMapOf<Int, Double?>()
 
         /**
         This operator tries to cast a service instance to a SensorManger-type
@@ -36,32 +39,65 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         - Then let executes a block of code on the sensor with it as this particular sensor object
         - In the block we store a reference to the sensor in question
          */
+
         sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.let {
-            // Log.d("sensor", "$accelerometer")
             this.accelerometer = it
             Log.d("sensor", "$accelerometer")
+            Log.d("sensor", "Sensor.TYPE_ACCELEROMETER: ${Sensor.TYPE_ACCELEROMETER}")
         }
+
     }
 
+    override fun onResume(){
+        super.onResume()
+        sensorManager.registerListener(this, accelerometer,SensorManager.SENSOR_DELAY_FASTEST)
+    }
 
-    @SuppressLint("SetTextI18n")
+    override fun onPause(){
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
     override fun onSensorChanged(event: SensorEvent?) {
-        Log.d("sensor-changed", "$event")
+        // Log.d("sensor", "onSensorChanged sensor: ${event.sensor}")
+
         when (event?.sensor?.type) {
-            Sensor.TYPE_ACCELEROMETER -> {
+            accelerometer?.type -> {
+
+                Log.d("sensor", "accelerometer changed: $accelerometer")
+
+
                 val alpha = 0.8f
 
-                accelGravity[0] = alpha * accelGravity[0] + (1 - alpha) * event.values[0]
-                accelGravity[1] = alpha * accelGravity[1] + (1 - alpha) * event.values[1]
-                accelGravity[2] = alpha * accelGravity[2] + (1 - alpha) * event.values[2]
 
-                accelLin[0] = event.values[0] - accelGravity[0]
-                accelLin[1] = event.values[1] - accelGravity[1]
-                accelLin[2] = event.values[2] - accelGravity[2]
 
-                this.tvAccelerometerX.text = "X: ${accelLin[0].toString()}"
-                this.tvAccelerometerY.text = "Y: ${accelLin[1].toString()}"
-                this.tvAccelerometerZ.text = "Z: ${accelLin[2].toString()}"
+                if(event != null){
+
+                    Log.d("sensor","SensorX changed: ${event.values[0]}")
+                    Log.d("sensor","SensorY changed: ${event.values[1]}")
+                    Log.d("sensor","SensorZ changed: ${event.values[2]}")
+
+                    this.tvAccelerometerX.text = "X: ${event.values[0]}"
+                    this.tvAccelerometerY.text = "Y: ${event.values[1]}"
+                    this.tvAccelerometerZ.text = "Z: ${event.values[2]}"
+
+                    /*
+                    accelGravity[0] = alpha * accelGravity[0]!! + (1 - alpha) * event.values[0]
+                    accelGravity[1] = alpha * accelGravity[1]!! + (1 - alpha) * event.values[1]
+                    accelGravity[2] = alpha * accelGravity[2]!! + (1 - alpha) * event.values[2]
+
+                    accelLin[0] = event.values[0] - accelGravity[0]!!
+                    accelLin[1] = event.values[1] - accelGravity[1]!!
+                    accelLin[2] = event.values[2] - accelGravity[2]!!
+
+                    this.tvAccelerometerX.text = "X: ${accelLin[0]}"
+                    this.tvAccelerometerY.text = "Y: ${accelLin[1]}"
+                    this.tvAccelerometerZ.text = "Z: ${accelLin[2]}"
+
+                    */
+
+                }
+
             }
         }
     }
