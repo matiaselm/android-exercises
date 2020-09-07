4ea02b21a6247a62be19c5de2bbf2e2edfc75942
Matias Jalava <matias@elm.fi>
2020-09-07 15:19:29 +0300

    tried to get animation to work, no success

--- app/src/main/java/com/example/exercises/MainActivity.kt
+++ app/src/main/java/com/example/exercises/MainActivity.kt
@@ -1,33 +1,37 @@
 package com.example.exercises
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), SensorEventListener {
+
+open class MainActivity : AppCompatActivity(), SensorEventListener {
 
     /*source: https://expertise.jetruby.com/how-to-implement-motion-sensor-in-a-kotlin-app-b70db1b5b8e5 */
     private lateinit var sensorManager: SensorManager
     private var accelerometer: Sensor? = null
+    private var pos: Boolean = false
+    private var screenHeight = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /**
+
+        /*
         This operator tries to cast a service instance to a SensorManger-type
         -> returns null if service is not an instance of SensorManager OR getSystemService returned null itself.
          */
-        this.sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        this.sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
 
-        /**
+        /*
         Used standard Kotlin library function let{}, in order not to overload the code with null checks. <- a good way to do this when using more than one sensor
         - Check whether the returned sensor is not null
         - Then let executes a block of code on the sensor with it as this particular sensor object
@@ -39,15 +43,45 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             Log.d("sensor", "$accelerometer")
             Log.d("sensor", "Sensor.TYPE_ACCELEROMETER: ${Sensor.TYPE_ACCELEROMETER}")
         }
+    }
+
+    // Point of this function is to apply animation to the screen showing the value according to them
+    private fun animate(X: Float, Y: Float, Z: Float) {
+
+        Log.d("sensor","animate start")
+
+        /*
+        val valueAnimator = ValueAnimator.ofFloat(0f, -screenHeight)
+
+        valueAnimator.addUpdateListener {
+            val value = it.animatedValue as Float
+            coordinateLayout.translationY = value
+        }
+
+        valueAnimator.interpolator = LinearInterpolator()
+        valueAnimator.duration = 2500L
+
+        valueAnimator.start()
+
+        */
+
+        /*
+        val animation = AnimationUtils.loadAnimation(this, R.anim.slide_up)
+        coordinateLayout.startAnimation(animation)
+        */
 
     }
 
-    override fun onResume(){
+    override fun onResume() {
         super.onResume()
-        sensorManager.registerListener(this, accelerometer,SensorManager.SENSOR_DELAY_FASTEST)
+
+        val displayMetrics = DisplayMetrics()
+        screenHeight = displayMetrics.heightPixels.toFloat()
+
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_FASTEST)
     }
 
-    override fun onPause(){
+    override fun onPause() {
         super.onPause()
         sensorManager.unregisterListener(this)
     }
@@ -57,8 +91,8 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
         when (event?.sensor?.type) {
             accelerometer?.type -> {
-                Log.d("sensor", "accelerometer changed: $accelerometer")
-                if(event != null){
+                // Log.d("sensor", "accelerometer changed: $accelerometer")
+                if (event != null) {
 
                     /*
                     Log.d("sensor","SensorX changed: ${event.values[0]}")
@@ -68,6 +102,8 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
                     this.tvAccelerometerX.text = "X: ${event.values[0]}"
                     this.tvAccelerometerY.text = "Y: ${event.values[1]}"
                     this.tvAccelerometerZ.text = "Z: ${event.values[2]}"
+
+                    animate(event.values[0], event.values[1], event.values[2])
 
                 }
 
