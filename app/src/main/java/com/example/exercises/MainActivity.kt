e81b61ef4033207f8d4d82843ce8abcf861bae7e
Matias Jalava <matias@elm.fi>
2020-09-08 11:05:15 +0300

    current location fetch working

--- app/src/main/java/com/example/exercises/MainActivity.kt
+++ app/src/main/java/com/example/exercises/MainActivity.kt
@@ -1,45 +1,93 @@
 package com.example.exercises
 
 
+import android.Manifest
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.LocationManager
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    // val lm = getSystemService(Context.LOCATION_SERVICE) as LocationManager  // Supposed to change to google play location api
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var latitude: Double = 0.0
+    private var longitude: Double = 0.0
+    private val LOCATION_PERMISSION_REQ_CODE = 1000;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /*      Let's change this to google play location API
-        if ((Build.VERSION.SDK_INT >= 23 &&
-                    ContextCompat.checkSelfPermission(
-                        this,
-                        android.Manifest.permission.ACCESS_FINE_LOCATION
-                    ) !=
-                    PackageManager.PERMISSION_GRANTED)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        button.setOnClickListener {
+            getCurrentLocation()
+        }
+
+    }
+
+    private fun getCurrentLocation() {
+        // check the permission
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
         ) {
             ActivityCompat.requestPermissions(
                 this,
-                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
-                0
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQ_CODE
             )
+
+            return
         }
 
-        lm.requestLocationUpdates(
-            LocationManager.GPS_PROVIDER,
-            60 * 1000,
-            50f,
-            this
-        )*/
+        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+            latitude = location.latitude
+            longitude = location.longitude
 
+            Log.d("GEOLOCATION", "latitude: $latitude \n longitude: $longitude")
+        }
+            .addOnFailureListener {
+                Toast.makeText(
+                    this, "Failed on getting current location",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>,grantResults: IntArray
+    ){
+        when (requestCode){
+            LOCATION_PERMISSION_REQ_CODE -> {
+                if (grantResults.isNotEmpty() &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    // permission granted
+                } else {
+                    // permission denied
+                    Toast.makeText(this,"You need to grant permission to access location",
+                    Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun openMap(){
+        val uri = Uri.parse("geo:$latitude,$longitude")
+        val mapIntent = Intent(Intent.ACTION_VIEW, uri)
+        mapIntent.setPackage("com.google.android.apps.maps")
+        startActivity(mapIntent)
     }
 }
 
