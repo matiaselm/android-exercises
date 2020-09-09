e2abe36e64522e2e44d37bf0a5addf9b74431467
Matias Jalava <matias@elm.fi>
2020-09-09 14:35:55 +0300

    a small start

--- app/src/main/java/com/example/exercises/MainActivity.kt
+++ app/src/main/java/com/example/exercises/MainActivity.kt
@@ -1,10 +1,33 @@
 package com.example.exercises
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 
 
 class MainActivity : AppCompatActivity() {
+
+    val REQUEST_IMAGE_CAPTURE = 99
+
+    val myIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+    private fun startIntent(){
+        if(myIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(myIntent, REQUEST_IMAGE_CAPTURE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, recIntent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, recIntent)
+
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK){
+            val extras = recIntent!!.extras
+            val imageBitmap = extras!!.get("data") as Bitmap
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
